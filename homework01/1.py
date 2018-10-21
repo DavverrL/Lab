51817341111ba5eb634dca664350f2b650f2b98c
ElzSurina <44273351+ElzSurina@users.noreply.github.com>
2018-10-21 16:37:18 +0300

    "Реализована функция is_prime(n)"

---
+++ homework01/1.py
@@ -0,0 +1,12 @@
+def is_prime(n):
+    """
+    >>> is_prime(2)
+    True
+    >>> is_prime(11)
+    True
+    >>> is_prime(8)
+    False
+    """
+    if (n / 2 != 0): True
+    else: False
+    pass
